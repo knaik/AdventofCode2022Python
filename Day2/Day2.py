facfaab24b504cceb3f079adbe69cfd0bca14d9f
knaik <kn172@rutgers.edu>
2022-12-06 16:27:46 -0500

    logic error, modulo operator return wrong move

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -19,7 +19,7 @@ you = ["X", "Y", "Z"]
 # lookup index then math? if win, -1 or -2?, same index = tied, check for warp around, otherwise positive = lose, negative = win
 #opp.index - you.index, first check equal, second check warp around
 
-playscut = plays[0:5]
+playscut = plays[0:45]
 #print(playscut) ## ['C Z', 'A Y', 'C Z', 'A Y', 'C Y']
                     #d      w       d       w       l
 #commited to checking win based on math and index search, assuming math is quick for single digits and index search in array is quick, maybe enumeration would have been better?
@@ -59,19 +59,32 @@ points = 0
 for e in plays:
     movecond = e.split()
     # add points from win draw lose
-    points = points + 3*you.index(movecond[1]) # using you as lose, draw, win instead of move 
+    print(movecond)
+    #print(movecond[1])
+    #print(3*you.index(movecond[1]))
+    points = points + (3*you.index(movecond[1])) # using you as lose, draw, win instead of move 
+    
+   
     # add points from their move shifted, based on draw lose win
-    # on lose do nothing
+    # on lose do nothing!!! wrong, still need to add moves
     if (you.index(movecond[1]) == 0):
+        yourmove = (((opp.index(movecond[0])) -1 +3) % 3) # forgot to add points based on move that lead to loss
+        points = points + yourmove + 1
+        #print(yourmove)
+        #print("lose")
         continue
     # on draw, same move
     elif (you.index(movecond[1]) == 1):
         points = points + opp.index(movecond[0]) + 1 # adding value of move, index + 1 since moves in order
+        #print("draw")
+        #print(opp.index(movecond[0]) + 1)
         continue
     elif (you.index(movecond[1]) == 2):
+        #print("win")
         #can use modulo 2? shift based on moves index of opponent + 1?
         # r->p, p->s, s->r, 01, 12, 2(3)0
-        yourmove = ((opp.index(movecond[0])) % 2) + 1 # add 1 mod 2
+        yourmove = ((opp.index(movecond[0]) + 1) % 3) # move index search based on win condition
+        #print(yourmove)
         points = points + yourmove + 1 # don't need to find by index again
         continue
     else: #primitive exception find

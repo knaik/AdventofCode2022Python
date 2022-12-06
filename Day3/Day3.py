b277eefdbf8e0a4528af090d060f1e3028f69a6e
knaik <kn172@rutgers.edu>
2022-12-06 17:47:58 -0500

    day 3 part 1

---
+++ Day3/Day3.py
@@ -0,0 +1,69 @@
+#Day 3 Aoc
+
+text = open("Day3\Day3.txt", "r")
+sacks = text.read().splitlines()
+text.close()
+
+
+sackscut = sacks[0:5]
+print(sackscut)
+
+sum = 0
+for singlesack in sacks:
+    midpt = len(singlesack)//2
+    splitone = sorted(singlesack[:midpt]) #close index not inclusive? split
+    print(splitone)
+    splittwo = sorted(singlesack[midpt:])
+    print(splittwo)
+    pointerone = 0
+    pointertwo = 0
+    prior = 0
+
+
+
+    while (splitone[pointerone] != splittwo[pointertwo]):  ##no error checking, bad but expect to always find match
+        print(splitone[pointerone])
+        print(splittwo[pointertwo])
+        if (splitone[pointerone] < splittwo[pointertwo]):
+            pointerone += 1 
+        elif(splitone[pointerone] > splittwo[pointertwo]): # AGHHHHH error for a while because forgot to switch sign
+            pointertwo += 1
+        else:
+            print("error2")#exceptiontwo
+            
+        if(pointerone > midpt or pointertwo > midpt): #simple exception
+            print("error")
+        if (splitone[pointerone] == splittwo[pointertwo]):
+            #found
+            foundchar = splitone[pointerone]
+            if (foundchar.isupper()):
+                prior = ord(foundchar) - 64 + 26 #get priority based on unicode char value ## add 26 to upper to get right number
+            else:
+                prior = ord(foundchar) - 96
+            print(foundchar)
+            break
+        if (splitone[pointerone] == splittwo[pointertwo]):
+            #found
+            foundchar = splitone[pointerone]
+            if (foundchar.isupper()):
+                prior = ord(foundchar) - 64 + 26
+            else:
+                prior = ord(foundchar) - 96
+            print(foundchar)
+            break
+    
+
+    if (splitone[pointerone] == splittwo[pointertwo]):
+        foundchar = splitone[pointerone]
+        if (foundchar.isupper()):
+            prior = ord(foundchar) - 64  + 26 #get priority based on unicode char value
+        else:
+            prior = ord(foundchar) - 96
+        print(foundchar)
+        print(prior)
+        sum = sum + prior
+        continue
+
+    print(prior)
+    sum = sum + prior
+print(sum)

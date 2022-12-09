4382bfb7b94fda4d19bf2ef1f39f8d4928ecfe41
knaik <kn172@rutgers.edu>
2022-12-08 21:34:32 -0500

    day 8

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -9,20 +9,32 @@ text.close()
 #print(trees)
 
 gridone = np.zeros_like(trees, int)
-print(gridone) #initialize zero
+#print(gridone) #initialize zero
 
 for j in range(1):
     for x,y in np.ndindex(trees.shape):   # for each x,y coordinate do
-            print(x,y)
-            print(trees[x, y+1:])
-            print(trees[x, y])
-            less = trees[x, y+1:] < trees[x, y] #count when smaller
-            #print(less)
-            gridone[x,y] = gridone[x,y] | all(less)
-            #print(all(less))  #bitwise or operator
+        """  print(x,y)
+        print(trees[x, y+1:])
+        print(trees[x, y]) """
+        less = trees[x, y+1:] < trees[x, y] #count when smaller
+        #print(less)
+        gridone[x,y] = gridone[x,y] | all(less)
+        #print(all(less))  #bitwise or operator
     trees, gridone = map(np.rot90, [trees, gridone]) #rotate with map
-    print(trees)
-    print(gridone)
+"""     print(trees)
+    print(gridone) """
 
 print(gridone.sum())
 
+
+
+gridtwo = np.ones_like(trees, int)
+#print(gridtwo)
+
+for k in range(4):
+    for x,y in np.ndindex(trees.shape):   
+        lower = trees[x, y+1:] < trees[x, y]
+        gridtwo[x,y] *= next((i+1 for i,t in enumerate(lower) if ~t), len(lower))
+    trees, gridtwo = map(np.rot90, [trees, gridtwo])
+
+print(gridtwo.max())

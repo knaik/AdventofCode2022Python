c05c96dcd147ec2168aee25647b3aa657abdb4e1
knaik <kn172@rutgers.edu>
2022-12-08 21:32:03 -0500

    day 8

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -1,8 +1,28 @@
 import numpy as np
 
+
 text = open("Day8\Day8.txt", "r")
-trees = np.array([list(x.strip()) for x in text], int) #can't use normal array, not big enough
+trees = np.array([list(x.strip()) for x in text], int) #don't want to use normal array, not really big enough
 text.close()
 
 
-print(trees)
+#print(trees)
+
+gridone = np.zeros_like(trees, int)
+print(gridone) #initialize zero
+
+for j in range(1):
+    for x,y in np.ndindex(trees.shape):   # for each x,y coordinate do
+            print(x,y)
+            print(trees[x, y+1:])
+            print(trees[x, y])
+            less = trees[x, y+1:] < trees[x, y] #count when smaller
+            #print(less)
+            gridone[x,y] = gridone[x,y] | all(less)
+            #print(all(less))  #bitwise or operator
+    trees, gridone = map(np.rot90, [trees, gridone]) #rotate with map
+    print(trees)
+    print(gridone)
+
+print(gridone.sum())
+
